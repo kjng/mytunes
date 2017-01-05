e54130d361262bc460d65b47b011988170eda58e
Kevin Jang <pianistkevinjang@gmail.com>
2017-01-05 12:28:08 -0800

    Get songs from parse server

--- client/data/data.js
+++ client/data/data.js
@@ -1,23 +1,40 @@
 // data.js - Defines an array of data regarding song files and their accompanying details.
-var songData = [
-  {
-    url: 'https://s3-us-west-1.amazonaws.com/hr-mytunes/data/04+One+In+A+Million.mp3',
-    title: 'One In A Million',
-    artist: 'Aaliyah',
+// var songData = [
+//   {
+//     url: 'https://s3-us-west-1.amazonaws.com/hr-mytunes/data/04+One+In+A+Million.mp3',
+//     title: 'One In A Million',
+//     artist: 'Aaliyah',
+//   },
+//   {
+//     url: 'https://s3-us-west-1.amazonaws.com/hr-mytunes/data/03+Age+Ain%27t+Nothing+But+A+Number.mp3',
+//     title: 'Age Ain\'t Nothing But A Number',
+//     artist: 'Aaliyah',
+//   },
+//   {
+//     url: 'https://s3-us-west-1.amazonaws.com/hr-mytunes/data/05+Hot+Like+Fire.mp3',
+//     title: 'Hot Like Fire',
+//     artist: 'Aaliyah',
+//   },
+//   {
+//     url: 'https://s3-us-west-1.amazonaws.com/hr-mytunes/data/06+If+Your+Girl+Only+Knew.mp3',
+//     title: 'If Your Girl Only Knew',
+//     artist: 'Aaliyah',
+//   }
+// ];
+
+//console.log($.get('https://api.parse.com/1/classes/songs'));
+
+var songData;
+$.ajax({
+  async: false,
+  url: 'https://api.parse.com/1/classes/songs',
+  type: 'GET',
+  contentType: 'application/json',
+  success: function (incoming) {
+    console.log('You got ' + incoming.results);
+    songData = incoming.results;
   },
-  {
-    url: 'https://s3-us-west-1.amazonaws.com/hr-mytunes/data/03+Age+Ain%27t+Nothing+But+A+Number.mp3',
-    title: 'Age Ain\'t Nothing But A Number',
-    artist: 'Aaliyah',
-  },
-  {
-    url: 'https://s3-us-west-1.amazonaws.com/hr-mytunes/data/05+Hot+Like+Fire.mp3',
-    title: 'Hot Like Fire',
-    artist: 'Aaliyah',
-  },
-  {
-    url: 'https://s3-us-west-1.amazonaws.com/hr-mytunes/data/06+If+Your+Girl+Only+Knew.mp3',
-    title: 'If Your Girl Only Knew',
-    artist: 'Aaliyah',
+  error: function (incoming) {
+    console.error('Failed to get songs', incoming);
   }
-];
+});
